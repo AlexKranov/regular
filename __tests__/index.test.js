fb660f4d5d001d096f2887e2db6f6d5879753ecb
AlexKranov <alexkranov@mail.ru>
2022-02-06 00:44:06 +0300

    *

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,27 +1,41 @@
 import Validator from '../src';
 
 test('Validator constructor1', () => {
-  const validator = new Validator('igor');
+  const validator = new Validator('Alex');
   expect(validator.validateUsername()).toBe(true);
 });
 
 test('Validator constructor2', () => {
-  const validator = new Validator('Коля');
+  const validator = new Validator('Алексей');
   expect(validator.validateUsername()).toBe(false);
 });
 
 test('Validator constructor3', () => {
-  const validator = new Validator('ddvdv999scsc');
+  const validator = new Validator('alexey111kranov');
   expect(validator.validateUsername()).toBe(true);
 });
 
+test('Validator constructor8', () => {
+  const validator = new Validator('alex11111111Кранов');
+  expect(validator.validateUsername()).toBe(false);
+});
 
 test('Validator constructor4', () => {
-  const validator = new Validator('5petya');
+  const validator = new Validator('1Alexey');
   expect(validator.validateUsername()).toBe(false);
 });
 
 test('Validator constructor5', () => {
-  const validator = new Validator('vasya5');
+  const validator = new Validator('Alexeyn1');
+  expect(validator.validateUsername()).toBe(false);
+});
+
+test('Validator constructor6', () => {
+  const validator = new Validator('-vasya');
+  expect(validator.validateUsername()).toBe(false);
+});
+
+test('Validator constructor9', () => {
+  const validator = new Validator('vasya-');
   expect(validator.validateUsername()).toBe(false);
 });
